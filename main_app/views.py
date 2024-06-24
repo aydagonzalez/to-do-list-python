36ab959d5a652e407eded838e353cc01ea1afeee
Ayda Gonzalez <aydagonzalez15@gmail.com>
2024-06-24 17:01:10 -0400

    added update fxn to main pg wo form

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Todo
 from django.views.generic import ListView
@@ -27,6 +27,37 @@ class TodoUpdate(UpdateView):
     model = Todo
     fields = '__all__'
 
-class TodoDelete(DeleteView):
-    model = Todo
-    success_url = '/'
+# class TodoDelete(DeleteView):
+#     model = Todo
+#     success_url = '/'
+
+
+# listings/views.py
+
+def todo_update(request, pk):
+    # pass
+    print("HELLOOOOOOOOOOOO !")
+    print('THIS IS THE REQUEST:',request, "THIS IS PK", pk)
+    todo = Todo.objects.get(id=pk)
+    form = TodoForm(instance = todo) # prepopulates the form with an existing data
+    return render(request,
+                  'home.html',
+                  {'form': form})
+    
+
+
+
+def todo_delete(request, pk):
+    # print("HELLOOOOOOOOOOOO !")
+    # print('THIS IS THE REQUEST:',request, "THIS IS PK", pk)
+    todo = Todo.objects.get(id=pk) # we need this for both GET and POST
+    # print("GOOOOOOOOODBYE!")
+    # print (todo)
+    if request.method == 'POST':
+        # delete the band from the database
+        todo.delete()
+        # redirect to the bands list
+        return redirect('home')
+    # no need for an `else` here. If it's a GET request, just continue
+    return render(request,
+                    'home.html')
